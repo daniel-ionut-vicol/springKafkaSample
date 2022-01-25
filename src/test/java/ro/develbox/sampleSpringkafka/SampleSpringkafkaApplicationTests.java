3c3ad4d28518e1b20eb03947e93c344fd6e7524a
daniel.ionut <daniel.ionu.vicol@gmail.com>
2022-01-25 22:41:44 +0200

    added spring boot starter project

---
+++ src/test/java/ro/develbox/sampleSpringkafka/SampleSpringkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.develbox.sampleSpringkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
