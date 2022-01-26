f05564dbc1ef73439966d470a08c87995fad2f66
daniel.ionut <daniel.ionu.vicol@gmail.com>
2022-01-26 19:34:37 +0200

    Revert "embedded keycloak into the spring boot app"
    This reverts commit 72d509ae8ff85baeba531d8909dc2688f9f085e0.

--- src/main/java/ro/develbox/sampleSpringkafka/SampleSpringkafkaApplication.java
+++ src/main/java/ro/develbox/sampleSpringkafka/SampleSpringkafkaApplication.java
@@ -1,39 +1,13 @@
 package ro.develbox.sampleSpringkafka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 
-import ro.develbox.sampleSpringkafka.security.keycloak.KeycloakServerProperties;
-
-@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
-@EnableConfigurationProperties({ KeycloakServerProperties.class })
+@SpringBootApplication
 public class SampleSpringkafkaApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(SampleSpringkafkaApplication.class);
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringkafkaApplication.class, args);
-	}
-
-	@Bean
-	ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties,
-			KeycloakServerProperties keycloakServerProperties) {
-
-		return (evt) -> {
-
-			Integer port = serverProperties.getPort();
-			String keycloakContextPath = keycloakServerProperties.getContextPath();
-
-			LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
-		};
 	}
 
 }
--- src/main/java/ro/develbox/sampleSpringkafka/security/keycloak/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package ro.develbox.sampleSpringkafka.security.keycloak;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
